eee90ec405900f25e21d3193e9bb0a69f13f5597
dpsabc@gmail.com <davips@users.noreply.github.com>
2014-06-21 00:01:43 -0300

    improved example and removed field 'size'

---
+++ src/main/scala/OSELMExample.scala
@@ -0,0 +1,61 @@
+import ml.classifiers._
+import util.{Tempo, Datasets}
+
+/*
+elm-scala: an implementation of ELM in Scala using MTJ
+Copyright (C) 2014 Davi Pereira dos Santos
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+object OSELMExample extends App {
+  println( """
+ elm-scala Copyright (C) 2014 Davi Pereira dos Santos
+
+ This program comes with ABSOLUTELY NO WARRANTY.
+ This is free software, and you are welcome to redistribute it
+ under certain conditions.
+ Refer to LICENSE file for details.     """)
+
+  val data = Datasets.arff(bina = true)("banana.arff") match {
+    case Right(x) => x
+    case Left(str) => println("Could not load iris dataset from the program path: " + str); sys.exit(0)
+  }
+  val currentSeed = (System.currentTimeMillis() % 1000).toInt
+
+  val elms = Seq(
+    IELM(initialL = 80, seed = currentSeed),
+    EIELM(initialL = 80, seed = currentSeed),
+    CIELM(initialL = 80, seed = currentSeed),
+    ECIELM(initialL = 80, seed = currentSeed),
+    OSELM(L = 16, seed = currentSeed)
+  )
+
+  println("Warming up JVM-BLAS interface...")
+  IELM(initialL = 15, seed = currentSeed).build(data)
+
+  Seq("iris", "banana") foreach { dataset =>
+    println("Comparing all ELMs in " + dataset + " dataset...")
+    elms foreach { elm =>
+      println(elm)
+
+      util.Datasets.kfoldCV(data, k = 10, parallel = true) { (trainingSet, testingSet, fold, _) =>
+        val (model, t) = Tempo.timev(elm.build(trainingSet))
+        val acc = model.accuracy(testingSet).formatted("%2.2f")
+        println("Fold " + fold + ": " + acc + " in " + t + "ms.")
+      }
+
+      println("")
+    }
+  }
+}
